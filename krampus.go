6fa8a6755394a77bc7841ea91e5a173d4f22fd23
Jan T. Sott <git@idleberg.com>
2024-04-10 00:34:05 +0200

    add command-line flags

--- krampus.go
+++ krampus.go
@@ -5,72 +5,108 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/alecthomas/kong"
+	"github.com/blang/semver"
 	"github.com/charmbracelet/log"
 	"github.com/shirou/gopsutil/net"
 	"github.com/shirou/gopsutil/process"
 )
 
+var Version string
+
+var CLI struct {
+	Ports []string `arg:"" default:""`
+	Version bool `short:"v" help:"Show version."`
+}
+
 var (
-		logger = log.NewWithOptions(os.Stderr, log.Options{
-				ReportTimestamp: false,
-		})
+	logger = log.NewWithOptions(os.Stderr, log.Options{
+		ReportTimestamp: false,
+	})
 )
 
 func main() {
-		ports := os.Args[1:]
-		for _, port := range ports {
-				pid, err := getPID(port)
+	kong.Parse(&CLI)
 
-				if pid == -1 {
-						continue
-				}
+	switch true {
+		case CLI.Version:
+			printVersion()
 
-				if err != nil {
-						logger.Error(err)
-						continue
-				}
+		default:
+			killPorts()
+	}
+}
 
-				err = killProcess(pid)
+func printVersion() {
+	ver, err := semver.Parse(Version)
 
-				if err != nil {
-						logger.Error(err)
-				}
+		var outputVersion string
+
+		if err == nil {
+			outputVersion = "v" + Version
+		} else {
+			outputVersion = ver.String()
 		}
+
+		fmt.Println(outputVersion)
+}
+
+func killPorts() {
+	ports := os.Args[1:]
+
+	for _, port := range ports {
+		pid, err := getPID(port)
+
+		if pid == -1 {
+			continue
+		}
+
+		if err != nil {
+			logger.Error(err)
+			continue
+		}
+
+		err = killProcess(pid)
+
+		if err != nil {
+			logger.Error(err)
+		}
+	}
 }
 
 func getPID(port string) (int32, error) {
-		conns, err := net.Connections("all")
-		if err != nil {
-				return 0, err
-		}
+	conns, err := net.Connections("all")
+	if err != nil {
+		return 0, err
+	}
 
-		portInt, err := strconv.Atoi(port)
-		if err != nil {
-				return 0, fmt.Errorf("invalid port %s", port)
-		}
+	portInt, err := strconv.Atoi(port)
+	if err != nil {
+		return 0, fmt.Errorf("invalid port %s", port)
+	}
 
-		for _, conn := range conns {
-				if conn.Status == "LISTEN" && conn.Laddr.Port == uint32(portInt) {
-						return conn.Pid, nil
-				}
+	for _, conn := range conns {
+		if conn.Status == "LISTEN" && conn.Laddr.Port == uint32(portInt) {
+			return conn.Pid, nil
 		}
+	}
 
-		logger.Warn(fmt.Sprintf("no process found listening on port %s", port))
-		return -1, nil
+	logger.Warn(fmt.Sprintf("no process found listening on port %s", port))
+	return -1, nil
 }
 
 func killProcess(pid int32) error {
-		proc, err := process.NewProcess(pid)
-		if err != nil {
-				return err
-		}
+	proc, err := process.NewProcess(pid)
+	if err != nil {
+		return err
+	}
 
-		err = proc.Kill()
-		if err != nil {
-				return err
-		}
+	err = proc.Kill()
+	if err != nil {
+		return err
+	}
 
-		logger.Info(fmt.Sprintf("killed process with PID %d", pid))
+	logger.Info(fmt.Sprintf("killed process with PID %d", pid))
 
-		return nil
+	return nil
 }
