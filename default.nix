3335b35f356630c90ab8aca96f2b31a01fc3ccad
Jan T. Sott <git@idleberg.com>
2026-03-24 00:01:27 +0100

    chore: version bump

--- default.nix
+++ default.nix
@@ -1,12 +1,11 @@
-{ pkgs }:
-
+{pkgs}:
 pkgs.buildGoModule rec {
   pname = "krampus";
-  version = "0.2.1";
+  version = "0.3.0";
 
   src = ./.;
 
   vendorHash = "sha256-3WfBvlM4aCwP9YqUWU4lnxpSLWHQNGWXjjg2F91awnY=";
 
-  ldflags = [ "-s" "-w" "-X=main.Version=${version}" ];
+  ldflags = ["-s" "-w" "-X=main.Version=${version}"];
 }
